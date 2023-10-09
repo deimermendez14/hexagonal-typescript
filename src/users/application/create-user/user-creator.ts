bf420a912c653d8c0d2ce427e8288b30b1d9c64e
deimermendez14 <deimer.javier.padilla.mendez@gmail.com>
2023-10-09 16:05:07 -0500

    feat: aplicate interface segragation principe

--- src/users/application/create-user/user-creator.ts
+++ src/users/application/create-user/user-creator.ts
@@ -2,15 +2,15 @@ import { UserEmail } from "src/users/domain/value-objects/user-email";
 import { UserId } from "src/users/domain/value-objects/user-id";
 
 import { User } from "../../domain/entities/user";
-import { UserRepository } from "../../domain/repositories/user-repository";
+import { UserSaver } from "../../domain/repositories/user-repository";
 import { createUser } from "./dto/create-user.dto";
 
 export class UserCreator {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(private readonly userSaver: UserSaver) {}
 
   async run({ email, id }: createUser): Promise<void> {
     const user = new User(new UserId(id), new UserEmail(email));
 
-    await this.userRepository.save(user);
+    await this.userSaver.save(user);
   }
 }
--- src/users/domain/repositories/user-repository.ts
+++ src/users/domain/repositories/user-repository.ts
@@ -1,10 +1,11 @@
 import { User } from "../entities/user";
 import { UserId } from "../value-objects/user-id";
 
-export interface UserRepository {
+export interface UserSaver {
+  save(user: User): Promise<void>;
+}
+export interface UserRepository extends UserSaver {
   get(): Promise<User[]>;
 
   getById(userId: UserId): Promise<User | null>;
-
-  save(user: User): Promise<void>;
 }
